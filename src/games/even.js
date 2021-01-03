3a22e724e6f3e35483b40e7a3986aae347a365ee
Alexander Leonov <spawnleon@gmail.com>
2021-01-03 21:24:18 +0300

    fix even game

--- src/games/even.js
+++ src/games/even.js
@@ -3,7 +3,7 @@ import { isEven, getIntRandomNumber } from '../utils/helpers.js';
 export default () => {
   const rules = 'Answer "yes" if the number is even, otherwise answer "no"';
   const currentRandomNumber = getIntRandomNumber();
-  const correctAnswer = isEven(getIntRandomNumber()) ? 'yes' : 'no';
+  const correctAnswer = isEven(currentRandomNumber) ? 'yes' : 'no';
   const question = `Question: ${currentRandomNumber}`;
   return { rules, correctAnswer, question };
 };
